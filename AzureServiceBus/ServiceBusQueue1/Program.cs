dabdc0e29eff00ba82368159a8bc6107ebbc9f1e
phuc.thai <phuc.thai@ecolecatholique.ca>
2025-06-11 19:58:38 +0700

    Add project files.

---
+++ AzureServiceBus/ServiceBusQueue1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string _connectionString = Environment.GetEnvironmentVariable("SB_CONNECTION_STRING");
+
+ArgumentNullException.ThrowIfNull(_connectionString, nameof(_connectionString));
+
+string queueName = "queue1";
+
+// since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+await using ServiceBusClient client = new(_connectionString);
+// create the sender
+ServiceBusSender sender = client.CreateSender(queueName);
+
+// create a message that we can send. UTF-8 encoding is used when providing a string.
+ServiceBusMessage message = new("Hello world!");
+
+// send the message
+await sender.SendMessageAsync(message);
+
+// create a receiver that we can use to receive the message
+ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+
+// the received message is a different type as it contains some service set properties
+ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+// get the message body as a string
+string body = receivedMessage.Body.ToString();
+Console.WriteLine(body);
---
+++ AzureServiceBus/ServiceBusQueue2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string _connectionString = Environment.GetEnvironmentVariable("SB_CONNECTION_STRING");
+
+ArgumentNullException.ThrowIfNull(_connectionString, nameof(_connectionString));
+
+string queueName = "queue1";
+
+// since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+await using ServiceBusClient client = new(_connectionString);
+// create the sender
+ServiceBusSender sender = client.CreateSender(queueName);
+
+// create a message that we can send. UTF-8 encoding is used when providing a string.
+ServiceBusMessage message = new($"Hello world! Schedule");
+message.ScheduledEnqueueTime = DateTimeOffset.UtcNow.AddSeconds(10); // Schedule the message to be sent in 10 seconds
+message.TimeToLive = TimeSpan.FromSeconds(5); // Expire at certain time
+
+// send the message
+await sender.SendMessageAsync(message);
