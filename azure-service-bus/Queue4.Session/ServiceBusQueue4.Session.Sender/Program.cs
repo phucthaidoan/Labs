49933ef4c16d61c1bb6118fb62a5904270024ff1
phuc.thai <phuc.thai@ecolecatholique.ca>
2025-06-22 17:16:48 +0700

    feat: Queue with session-enabled.

---
+++ azure-service-bus/Queue4.Session/ServiceBusQueue4.Session.Sender/Program.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text;
+
+string _connectionString = Environment.GetEnvironmentVariable("SB_CONNECTION_STRING");
+
+ArgumentNullException.ThrowIfNull(_connectionString, nameof(_connectionString));
+
+string queueName = "queue.session1";
+
+Console.WriteLine("Sending a message to queue...");
+
+// since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+await using ServiceBusClient client = new(_connectionString);
+// create the sender
+ServiceBusSender sender = client.CreateSender(queueName);
+
+for (var i = 1; i <= 200; i++)
+{
+
+    // create a session message that we can send
+    ServiceBusMessage message = new(Encoding.UTF8.GetBytes($"M{i}"))
+    {
+        SessionId = "mySessionId"
+    };
+
+    // send the message
+    await sender.SendMessageAsync(message);
+}
