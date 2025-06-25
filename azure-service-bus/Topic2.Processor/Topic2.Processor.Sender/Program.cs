9891f0ea5adcc1486ef55976b7d82ade73c8784b
Phuc Thai <bindirt@gmail.com>
2025-06-25 21:31:24 +0700

    Sample of using processor in azure service bus subscription (#4)

---
+++ azure-service-bus/Topic2.Processor/Topic2.Processor.Sender/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string _connectionString = Environment.GetEnvironmentVariable("SB_CONNECTION_STRING");
+
+ArgumentNullException.ThrowIfNull(_connectionString, nameof(_connectionString));
+
+string subscriptionName = "subscription1";
+string topicName = "topic1";
+
+// since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+await using ServiceBusClient client = new(_connectionString);
+// create the sender that we will use to send to our topic
+ServiceBusSender sender = client.CreateSender(topicName);
+
+// create a message that we can send. UTF-8 encoding is used when providing a string.
+ServiceBusMessage message = new("Hello world! ABC");
+
+// send the message
+await sender.SendMessageAsync(message);
+
+// create a receiver for our subscription that we can use to receive the message
+ServiceBusReceiver receiver = client.CreateReceiver(topicName, subscriptionName);
+
+// the received message is a different type as it contains some service set properties
+ServiceBusReceivedMessage receivedMessage = await receiver.ReceiveMessageAsync();
+
+// get the message body as a string
+string body = receivedMessage.Body.ToString();
+Console.WriteLine(body);
