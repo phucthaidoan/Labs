975529cdae1614f1535ae8211456dc7766157a3c
Phuc Thai <bindirt@gmail.com>
2025-09-03 13:43:52 +0700

    feat: using github models and Semantic Kernel (#8)
    Co-authored-by: phuc.thai <phuc.thai@ecolecatholique.ca>

---
+++ genAI/github-models/WithSemanticKernel/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using OpenAI;
+using System.ClientModel;
+using System.Text;
+
+// Create a chat completion service with a model from GitHub Models
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+var modelId = "microsoft/Phi-3.5-mini-instruct";
+var uri = "https://models.github.ai/inference";
+var githubPAT = config["GH_PAT"];
+
+// create client
+var client = new OpenAIClient(new ApiKeyCredential(githubPAT), new OpenAIClientOptions { Endpoint = new Uri(uri) });
+
+// Create a chat completion service
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(modelId, client);
+
+// Get the chat completion service
+Kernel kernel = builder.Build();
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+
+var history = new ChatHistory();
+history.AddSystemMessage("You are a useful chatbot. If you don't know an answer, say 'I don't know!'. Always reply in a funny ways. Use emojis if possible.");
+
+while (true)
+{
+    Console.Write("Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+    history.AddUserMessage(userQ);
+
+    var sb = new StringBuilder();
+    var result = chat.GetStreamingChatMessageContentsAsync(history);
+    Console.Write("AI: ");
+    await foreach (var item in result)
+    {
+        sb.Append(item);
+        Console.Write(item.Content);
+    }
+    Console.WriteLine();
+
+    history.AddAssistantMessage(sb.ToString());
+}
