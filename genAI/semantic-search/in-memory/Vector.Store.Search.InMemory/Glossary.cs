f9e708147f3963a390f0807c7b428ca79a2b29c7
Phuc Thai <bindirt@gmail.com>
2025-09-05 09:19:24 +0700

    feat - init simple in memory vector storage. (#9)
    Co-authored-by: phuc.thai <phuc.thai@ecolecatholique.ca>

---
+++ genAI/semantic-search/in-memory/Vector.Store.Search.InMemory/Glossary.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.VectorData;
+
+public class MyNamespace
+{
+    /// <summary>
+    /// Sample model class that represents a glossary entry.
+    /// </summary>
+    /// <remarks>
+    /// Note that each property is decorated with an attribute that specifies how the property should be treated by the vector store.
+    /// This allows us to create a collection in the vector store and upsert and retrieve instances of this class without any further configuration.
+    /// </remarks>
+    public sealed class Glossary
+    {
+        [VectorStoreKey]
+        public ulong Key { get; set; }
+
+        [VectorStoreData(IsIndexed = true)]
+        public string Category { get; set; }
+
+        [VectorStoreData]
+        public string Term { get; set; }
+
+        [VectorStoreData]
+        public string Definition { get; set; }
+
+        [VectorStoreVector(1536)]
+        public ReadOnlyMemory<float> DefinitionEmbedding { get; set; }
+    }
+
+    /// <summary>
+    /// Create some sample glossary entries.
+    /// </summary>
+    /// <returns>A list of sample glossary entries.</returns>
+    public static IEnumerable<Glossary> CreateGlossaryEntries()
+    {
+        yield return new Glossary
+        {
+            Key = 1,
+            Category = "External Definitions",
+            Term = "API",
+            Definition = "Application Programming Interface. A set of rules and specifications that allow software components to communicate and exchange data."
+        };
+
+        yield return new Glossary
+        {
+            Key = 2,
+            Category = "Core Definitions",
+            Term = "Connectors",
+            Definition = "Connectors allow you to integrate with various services provide AI capabilities, including LLM, AudioToText, TextToAudio, Embedding generation, etc."
+        };
+
+        yield return new Glossary
+        {
+            Key = 3,
+            Category = "External Definitions",
+            Term = "RAG",
+            Definition = "Retrieval Augmented Generation - a term that refers to the process of retrieving additional data to provide as context to an LLM to use when generating a response (completion) to a user’s question (prompt)."
+        };
+    }
+}
